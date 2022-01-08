ae24d6ee775c5e56800ae29c02dd62aa1bfe02dd
Ndhossain <95048995+Ndhossain@users.noreply.github.com>
2022-01-08 14:54:27 +0600

    work going on

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+// enter button
+
 const loginButton = document.getElementById('logIn');
 const logInArea = document.getElementById('logInField')
 const transictionArea = document.getElementById('transictionArea');
 loginButton.addEventListener('click', function () {
     logInArea.style.display = 'none';
     transictionArea.style.display = 'block';
+})
+
+// deposit button
+
+const depositButton = document.getElementById('depositBtn');
+
+depositButton.addEventListener('click', function () {
+    const depositAmount = document.getElementById('depoAmount').value;
+    const depositAmountNumber = parseFloat(depositAmount);
+});
+
+// withdraw button
+
+const withdrawButton = document.getElementById('withdrawBtn');
+
+withdrawButton.addEventListener('click', function () {
+    const withdrawAmount = document.getElementById('withAmount').value;
+    const withdrawAmountNumber = parseFloat(withdrawAmount);
 })
